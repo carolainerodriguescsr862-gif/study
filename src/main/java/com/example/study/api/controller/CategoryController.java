f56fa8e7f3ba10d649fcb1898c0edf17deb2b327
Carolaine-Rodrigues <carolainerodrigues.csr862@gmail.com>
2026-06-07 12:38:37 -0300

    feat: New functionalit

--- src/main/java/com/example/study/api/controller/CategoryController.java
+++ src/main/java/com/example/study/api/controller/CategoryController.java
@@ -2,30 +2,33 @@ package com.example.study.api.controller;
 
 
 import com.example.study.api.dto.CategoryRequestDTO;
+import com.example.study.api.dto.CategoryResponseDTO;
 import com.example.study.domain.service.CategoryService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-@RestController("/api")
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/categories")
 public class CategoryController {
     private final CategoryService categoryService;
 
     public CategoryController(CategoryService categoryService) {
         this.categoryService = categoryService;
     }
-    // get
-    @GetMapping("/list/all")
-    public ResponseEntity<?> listAll(){
+
+    @GetMapping
+    public ResponseEntity<List<CategoryResponseDTO>> listAll(){
         var category = categoryService.findAll();
         return ResponseEntity.ok(category);
     }
 
-    @PostMapping("/created")
-    public ResponseEntity<?> create(@RequestBody CategoryRequestDTO data){
+    @PostMapping
+    public ResponseEntity<CategoryResponseDTO> create(@Valid @RequestBody CategoryRequestDTO data){
         var category = categoryService.create(data);
-        return ResponseEntity.ok(category);
+        return ResponseEntity.status(HttpStatus.CREATED).body(category);
     }
 }
---
+++ src/main/java/com/example/study/api/controller/ProductController.java
@@ -0,0 +1,61 @@
+package com.example.study.api.controller;
+
+import com.example.study.api.dto.ProductPatchDTO;
+import com.example.study.api.dto.ProductRequestDTO;
+import com.example.study.api.dto.ProductResponseDTO;
+import com.example.study.api.dto.ProductUpdateDTO;
+import com.example.study.domain.service.ProductService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class ProductController {
+
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @PostMapping("/categories/{categoryId}/products")
+    public ResponseEntity<ProductResponseDTO> createProduct(@Valid @RequestBody ProductRequestDTO data, @PathVariable String categoryId){
+        var product = productService.create(data, categoryId);
+        return ResponseEntity.status(HttpStatus.CREATED).body(product);
+    }
+
+    @GetMapping("/products")
+    public ResponseEntity<List<ProductResponseDTO>> listAll(){
+        var products = productService.findAll();
+        return ResponseEntity.ok(products);
+    }
+
+    @GetMapping("/products/{id}")
+    public ResponseEntity<ProductResponseDTO> listId(@PathVariable String id){
+        var product = productService.findById(id);
+        return ResponseEntity.ok(product);
+    }
+
+    @PutMapping("/products/{id}")
+    public ResponseEntity<ProductResponseDTO> updateAll(@PathVariable String id, @Valid @RequestBody ProductUpdateDTO data){
+        var product = productService.update(id,data);
+        return ResponseEntity.ok(product);
+    }
+    @PatchMapping("/products/{id}")
+    public ResponseEntity<ProductResponseDTO> updatePartial(@PathVariable String id, @Valid @RequestBody ProductPatchDTO data){
+
+        var product = productService.updatePartial(id,data);
+        return ResponseEntity.ok(product);
+    }
+
+    @DeleteMapping("/products/{id}")
+    public ResponseEntity<Void> delete(@PathVariable String id){
+        productService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+}
