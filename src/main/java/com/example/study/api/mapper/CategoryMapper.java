374f71695eae23b9ef3f8f73e5b911fb92d539f7
Carolaine-Rodrigues <carolainerodrigues.csr862@gmail.com>
2026-06-13 11:20:43 -0300

    fix: resolve runtime error on application startup

--- src/main/java/com/example/study/api/mapper/CategoryMapper.java
+++ src/main/java/com/example/study/api/mapper/CategoryMapper.java
@@ -1,15 +1,26 @@
 package com.example.study.api.mapper;
 
 import com.example.study.api.dto.CategoryResponseDTO;
+import com.example.study.api.dto.ProductSummaryDTO;
 import com.example.study.domain.entity.Category;
+
+import java.util.List;
 
 public class CategoryMapper {
 
     public static CategoryResponseDTO toDTO(Category category){
+
+        List<ProductSummaryDTO> products = category.getProducts()
+                .stream()
+                .map(product -> new ProductSummaryDTO(
+                        product.getId(),
+                        product.getName()
+                )).toList();
+
        return new CategoryResponseDTO(
                 category.getId(),
                 category.getName(),
-                category.getProducts()
+                products
         );
     }
 }
